66307a909d9d6786d5e161b78cb6f559e657dd12
aiaja <aisyafirst@gmail.com>
2025-04-30 15:10:47 +0700

    Dashboard title

--- src/app/(dashboard)/tenants/home/page.tsx
+++ src/app/(dashboard)/tenants/home/page.tsx
@@ -3,6 +3,7 @@ import { SectionCards } from "@/components/home";
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-4">
+      <h1 className="px-6 text-xl font-bold">DASHBOARD</h1>
       <SectionCards />
       
     </div>
