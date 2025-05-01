1d6ef25274ffae93e1df0c956485fa0579132fb4
annisanugraha <111202315345@mhs.dinus.ac.id>
2025-05-01 21:20:55 +0700

    className

--- src/components/products-table.tsx
+++ src/components/products-table.tsx
@@ -39,7 +39,7 @@ export function ProductsTable() {
         <Table>
 
           {/* Search Bar Row */}
-          <TableHeader className="right">
+          <TableHeader className="w-full col-span-4">
             <TableRow>
               <TableCell colSpan={12} className="px-4 py-2">
                 <Search
