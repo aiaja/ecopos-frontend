07d2f1184427540ddd065135778dc4dc02d9ec47
annisanugraha <111202315345@mhs.dinus.ac.id>
2025-05-02 11:36:47 +0700

    configuring permission

--- src/app/(dashboard)/tenants/user-management/permission/page.tsx
+++ src/app/(dashboard)/tenants/user-management/permission/page.tsx
@@ -1,4 +1,9 @@
-export default function Page() {
-    return <h1>Permission page!</h1>
+import { PermissionTable } from "@/components/permission-table";
+
+export default function PermissionPage() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <PermissionTable />
+    </div>
+  );
 }
-  
