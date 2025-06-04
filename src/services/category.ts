f32da61658598bfea535caa3f290d68576ca5af0
Jong44 <mat.skak45@gmail.com>
2025-06-04 23:49:37 +0700

    update category fetch

---
+++ src/services/category.ts
@@ -0,0 +1,95 @@
+import { BASE_URL } from "./BASE_URL"
+import axios from "axios";
+
+type Category = {
+    id?: string;
+    name: string;
+    outlet_id?: string;
+}
+
+
+const getCategories = async (outletId: string): Promise<Category[]> => {
+    const response = await fetch(`${BASE_URL}/outlets/${outletId}/categories`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json',
+        },
+    }
+    );
+    if (response.status === 500) {
+        throw new Error('Failed to fetch categories');
+    }
+    const data = await response.json();
+    return data.categories;
+}
+
+const getCategoryById = async (outletId:string, id: string): Promise<Category> => {
+    const token = `Bearer ${localStorage.getItem('token')}`;
+    const response = await axios.get(`${BASE_URL}/outlets/${outletId}/categories/${id}`, {
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.data || !response.data.category) {
+        throw new Error('Category not found');
+    }
+    const data = await response.data;
+    return data.category;
+
+}
+
+const createCategory = async (outletId: string, category: Omit<Category, 'id'>): Promise<Category> => {
+    const token = `Bearer ${localStorage.getItem('token')}`;
+    const response = await axios.post(`${BASE_URL}/outlets/${outletId}/categories`, {
+        ...category,
+        outlet_id: outletId,
+    }, {
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.data || !response.data.category) {
+        throw new Error('Failed to create category');
+    }
+    const data = await response.data;
+    return data.category;
+}
+
+const updateCategory = async (outletId: string, id: string, category: Omit<Category, 'id'>): Promise<Category> => {
+    const token = `Bearer ${localStorage.getItem('token')}`;
+    const response = await axios.put(`${BASE_URL}/outlets/${outletId}/categories/${id}`, {
+        ...category,
+        outlet_id: outletId,
+    }, {
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.data || !response.data.category) {
+        throw new Error('Failed to update category');
+    }
+    const data = await response.data;
+    return data.category;
+}
+
+const deleteCategory = async (outletId: string, id: number): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/outlets/${outletId}/categories/${id}`, {
+        method: 'DELETE',
+    });
+    if (!response.ok) {
+        throw new Error('Failed to delete category');
+    }
+}
+
+export const CategoryService = {
+    getCategories,
+    getCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+}
+
