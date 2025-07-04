0ac651f09453e9790c43145b212bc6491c202221
aiaja <aisyafirst@gmail.com>
2025-07-04 21:52:09 +0700

    takeout cashier report

--- src/components/reports/reports-tabs.tsx
+++ src/components/reports/reports-tabs.tsx
@@ -59,12 +59,6 @@ export function ReportsTabs({
 
 
   return (
-    <Tabs defaultValue="account" className="w-full">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="Selling">Selling Report</TabsTrigger>
-        <TabsTrigger value="Cashier">Cashier Report</TabsTrigger>
-      </TabsList>
-      <TabsContent value="Selling">
         <Card>
           <CardContent className="space-y-2">
             <div className="flex justify-between">
@@ -93,35 +87,5 @@ export function ReportsTabs({
             </div>
           </CardContent>
         </Card>
-      </TabsContent>
-      
-      <TabsContent value="Cashier">
-        <Card>
-          <CardContent className="space-y-2">
-            <div className="flex justify-between">
-              <div className="space-y-1">
-                <Label htmlFor="date" className="pb-2">
-                  Pick Date Range
-                </Label>
-                <div className="flex items-center">
-                  </div>
-              </div>
-              <div className="flex gap-4">
-                <Button>Generate</Button>
-                <Button>
-                  <Printer className="w-4 h-4" />
-                  Print
-                </Button>
-                <Button>
-                  <Download className="w-4 h-4" />
-                  Download
-                </Button>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-      </TabsContent>
-      
-    </Tabs>
   );
 }
